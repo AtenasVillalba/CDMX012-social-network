3e9a36187ed9e91f503fd3ea0ffdd8f5c5b3c2af
Atenas Villalba <atenasvillalba8@gmail.com>
2022-04-21 09:31:59 -0500

    estilos css

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,13 +7,13 @@ export const profile = () => {
   const profileContent = document.createElement("img");
   profileContent.setAttribute(
     "src",
-    "../Resourses/Logos/404_Solovino_movile.jpg"
+    "./Resourses/Logos/404_Solovino_movile.jpg"
   );
   profileContent.className = "working-adoptions";
   profileContent.id = "image-working";
 
   const profileClose = document.createElement("img");
-  profileClose.setAttribute("src", "../Resourses/icons/flecha.png");
+  profileClose.setAttribute("src", "./Resourses/icons/flecha.png");
   profileClose.setAttribute("class", "Adopt_close");
   profileClose.addEventListener("click", () => {
     onNavigate("/Timeline");
